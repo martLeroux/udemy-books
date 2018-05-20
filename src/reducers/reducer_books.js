09a370f85097b232ac9e1d71beb8bf06dc4fb3ec
Martin Leroux <tintin_151@hotmail.com>
2018-05-20 18:32:25 -0400

    Créer l'application de livres

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function (){
+	return [
+		{ title: 'Javascript : The good Parts', pages: 101}, 
+		{ title: 'Harry Potter', pages: 202},
+		{ title: 'The dark tower', pages: 303},
+		{ title: 'Eloquent Ruby', pages: 404}  
+	]
+}
